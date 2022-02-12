9554797e5990f15dea5d33f81491a1ef58f25d90
David <dabn313@gmail.com>
2022-02-11 18:14:51 -0800

    adding 1-last_digit.c

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/**
+ * main - this function assign a random number
+ * executes and print the number
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+int n;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+printf("Last digit of %d is %d ", n, n % 10);
+/* your code goes there */
+if (n % 10 > 5)
+{
+printf("and is greater than 5\n");
+}
+else if (n % 10 == 0)
+{
+printf("and is 0\n");
+}
+else if (n % 10 < 6 && n % 10 != 0)
+{
+printf("and is less than 6 and not 0\5");
+}
+return (0);
+}
