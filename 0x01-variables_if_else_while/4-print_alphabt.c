b25825077186304d6e06b6b6bb22a15f1cd6c9af
David <dabn313@gmail.com>
2022-02-13 16:48:39 -0800

    adding 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -4,25 +4,24 @@
  * main - prints the alphabet in lowercase,
  * followed by a new line,
  * Print all the letters except q and e
- * You can only use the putchar function (every other function (printf, puts, etc…) is forbidden)
+ * You can only use the putchar function
  * All your code should be in the main function
  * You can only use putchar twice in your code
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-  char letter = 'a';
+char letter = 'a';
 
-  while (letter <= 'z')
-    ;
-  {
-    if (letter != 'e' && letter != 'q')
-    {
-      putchar(letter);
-    }
-    letter++;
-  }
-  putchar('\n')
+while (letter <= 'z')
+{
+if (letter != 'e' && letter != 'q')
+{
+putchar(letter);
+}
+letter++;
+}
+putchar('\n');
 
-      return (0);
+return (0);
 }
