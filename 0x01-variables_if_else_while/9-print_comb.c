2f0d068c6a989a4d2ee6904fae5150d664e3796d
David <dabn313@gmail.com>
2022-02-14 18:21:45 -0800

    adding 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinations,
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 48; num < 58; num++)
+	{
+		putchar(n);
+		if (num != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
