197670e48ffd593cc22a45ed4c69fd11e47234e6
David <dabn313@gmail.com>
2022-02-16 14:40:17 -0800

    adding

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
 /**
- * main - print _putchar,
+ * main - print _putchar
  * followed by a new line
- * Return: Always 0 (Success)
+ * Return: 0 if success
  */
 int main(void)
 {
+	_putchar("_");
 	_putchar("p");
 	_putchar("u");
 	_putchar("t");
@@ -15,5 +16,6 @@ int main(void)
 	_putchar("a");
 	_putchar("r");
 	_putchar("\n");
+
+	return (0);
 }
-return (0);
