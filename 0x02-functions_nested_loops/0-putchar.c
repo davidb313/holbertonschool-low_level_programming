1962691a3fa2fa2fbe2ac7f33523330bd54de74c
David <dabn313@gmail.com>
2022-02-16 03:59:42 -0800

    adding 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print _putchar,
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	_putchar("p");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("h");
+	_putchar("a");
+	_putchar("r");
+	putchar("\n");
+}
+return (0);
