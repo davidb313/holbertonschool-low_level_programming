0721a1fa525eefdc185525d5561837f0d9994e37
David <dabn313@gmail.com>
2022-02-17 18:25:54 -0800

    adding files

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * positive_or_negative - this function assign a random number
+ * executes and print the number
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+int i;
+
+if (i > 0)
+{
+printf("%d is positive\n", i);
+}
+else if (i == 0)
+{
+printf("%d is zero\n", i);
+}
+else
+{
+printf("%d is negative\n", i);
+}
+return (0);
+}
