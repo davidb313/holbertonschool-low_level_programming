4dcf7264033612b0b13999e3f69810724becd741
David <dabn313@gmail.com>
2022-02-17 18:07:36 -0800

    adding the files

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include <main.h>
+
+/**
+ * main - function to probe
+ * Return: 0 is success positive or negative
+ */
+
+int main(void)
+{
+int i;
+i = 0;
+positive_or_negative(i)
+
+return (0);
+}
