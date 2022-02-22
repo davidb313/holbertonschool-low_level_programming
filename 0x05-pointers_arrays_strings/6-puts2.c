c37bc5754b6af2418512ce943e092a62ded1ecb3
David <dabn313@gmail.com>
2022-02-21 20:00:40 -0800

    adding files

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string
+ * followed by a new line
+ * @str: string to print the chars from
+ */
+void puts2(char *str)
+{
+int i, lenght;
+
+lenght = 0;
+
+while (str[lenght] != '\0')
+{
+lenght++;
+}
+
+for (i = 0, i < lenght; i += 2)
+{
+_putchar(str[i]);
+}
+
+_putchar('\n');
+}
